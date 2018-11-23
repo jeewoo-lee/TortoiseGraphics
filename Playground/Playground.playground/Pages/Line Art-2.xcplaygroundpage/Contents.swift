cd5ad7c0c1ec535012b6049cf9468be37b0106b8
Jeewoo Lee <jlee@lcs.on.ca>
2018-11-23 10:46:58 -0500

    Line Art 2

---
+++ Playground/Playground.playground/Pages/Line Art-2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import PlaygroundSupport
+import TortoiseGraphics
+import CoreGraphics
+
+let myFrame = CGRect(x: 0, y: 0, width: 500, height: 500)
+let canvas = PlaygroundCanvas(frame: myFrame)
+canvas.frameRate = 30
+canvas.color = .white
+PlaygroundPage.current.liveView = canvas
+
+canvas.drawing { turtle in
+    
+    turtle.drawMyStar(withSize: 50)
+    
+}
--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -143,4 +143,19 @@ public extension Tortoise {
         self.right(120)
         self.forward(size)
     }
+    func drawMyStar (withSize size: Double) {
+        for _ in 1...3 {
+            for _ in 1...10 {
+                self.trapazoid(withSize: size)
+                //turtle.square(withSize: 45)
+                self.right(36)
+                for _ in 1...4 {
+                    self.triangle(withSize: (size - 10))
+                    self.circle(size/2.5)
+
+                }
+            }
+            self.right(20)
+        }
+    }
 }
